3b973843db693f5c15f5e110ddd16cb2be96efd3
Mauricio Pazos <mauricio.pazos@gmail.com>
2013-04-03 13:39:33 +0200

    adding geotools mif format

--- mapfishapp/src/main/java/org/georchestra/mapfishapp/ws/upload/GeotoolsFeatureReader.java
+++ mapfishapp/src/main/java/org/georchestra/mapfishapp/ws/upload/GeotoolsFeatureReader.java
@@ -5,12 +5,22 @@ package org.georchestra.mapfishapp.ws.upload;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.geotools.data.DataStore;
 import org.geotools.data.FileDataStore;
+import org.geotools.data.ServiceInfo;
+import org.geotools.data.mif.MIFDataStore;
+import org.geotools.data.mif.MIFDataStoreFactory;
 import org.geotools.data.shapefile.ShapefileDataStoreFactory;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.referencing.CRS;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.referencing.FactoryException;
 
 /**
  * This implementation is a façade to the Geotools store implementations.
@@ -45,10 +55,46 @@ class GeotoolsFeatureReader implements FeatureFileReaderImplementor {
 		case shp:
 			return readShpFile(basedir);
 			
+		case mif:
+			return readMifFile(basedir);
+			
 		default:
 			throw new IOException("Unsuported format: " + fileFormat.toString());
 		}
 		
+	}
+
+	/**
+	 * Reads the features from MIF file.
+	 * 
+	 * @param basedir
+	 * @return {@link SimpleFeatureCollection}
+	 * 
+	 * @throws IOException
+	 */
+	private SimpleFeatureCollection readMifFile(File basedir) throws IOException{
+		
+		HashMap params = new HashMap();
+//        try {
+//            Integer crs = CRS.lookupEpsgCode(schema.getCoordinateReferenceSystem(), true);
+//            params.put(MIFDataStore.PARAM_SRID, crs );
+//        } catch (FactoryException e) {
+//            LOG.warn("unable to convert "+schema.getCoordinateReferenceSystem()+" to a EPSG code", e);
+//        }
+        params.put(MIFDataStoreFactory.PARAM_PATH, basedir);
+		
+		MIFDataStoreFactory storeFactory = new MIFDataStoreFactory();
+		DataStore store = storeFactory.createDataStore(params);
+		
+		ServiceInfo info = store.getInfo();
+		URI uriSchema  = info.getSchema();
+		SimpleFeatureType schema = store.getSchema(uriSchema.toString());
+
+		SimpleFeatureSource featureSource = store.getFeatureSource(schema.getTypeName());
+		
+		SimpleFeatureCollection features = featureSource.getFeatures();
+		
+		return features;
 	}
 
 	/**
@@ -61,9 +107,9 @@ class GeotoolsFeatureReader implements FeatureFileReaderImplementor {
 	 */
 	private SimpleFeatureCollection readShpFile(final File basedir) throws IOException {
 		
-		ShapefileDataStoreFactory shpStoreFactory = new ShapefileDataStoreFactory();
+		ShapefileDataStoreFactory storeFactory = new ShapefileDataStoreFactory();
 		
-		FileDataStore store = shpStoreFactory.createDataStore(basedir.toURI().toURL());
+		FileDataStore store = storeFactory.createDataStore(basedir.toURI().toURL());
 
 		SimpleFeatureType schema = store.getSchema();
 		SimpleFeatureSource featureSource = store.getFeatureSource(schema.getTypeName());
